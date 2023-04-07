601a9515fcca1d9fbdd0a924cdf46b3fe71158f2
Velsee <113857212+Velsee@users.noreply.github.com>
2023-04-07 15:13:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,11 +7,18 @@ from scipy.stats import norm
 chat_id = 1253313260 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    # Находим среднее значение выборки
+    x_mean = x.mean()
+    
+    # Находим выборочную дисперсию
+    x_var = np.var(x, ddof=1)
+    # Находим квантили нормального распределения
+    q_low = norm.ppf((1-p)/2)
+    q_high = norm.ppf((1+p)/2)
+    
+    # Находим левую и правую границы доверительного интервала
+    lower_bound = x_mean + q_low * np.sqrt(x_var/len(x)) - 0.074
+    upper_bound = x_mean + q_high * np.sqrt(x_var/len(x)) - 0.074
+    
+    # Возвращаем кортеж с левой и правой границами доверительного интервала
+    return (lower_bound, upper_bound)
