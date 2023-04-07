5e52c306ddb319f1e033a04a774e9d9b2f7eac4b
Velsee <113857212+Velsee@users.noreply.github.com>
2023-04-07 23:51:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,7 +7,20 @@ from scipy.stats import norm
 chat_id = 1253313260 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    b = 0.074 + (1 - 0.074) * p
-    b_min = 0.074 + (1 - 0.074) * np.min(x)
-    b_max = 0.074 + (1 - 0.074) * np.max(x)
-    return (b_min, b_max)
+    # Расчет альфа
+    alpha = 1 - p
+
+    # Находим квантили стандартного нормального распределения
+    z_left = norm.ppf(alpha / 2)
+    z_right = norm.ppf(1 - alpha / 2)
+
+    # Расчитываем выборочное среднее и дисперсию
+    x_mean = np.mean(x)
+    s_squared = np.var(x, ddof=1)
+
+    # Расчет границ доверительного интервала
+    lower_bound = (x_mean - 0.074) - (z_right * ((s_squared / len(x)) ** 0.5))
+    upper_bound = (x_mean - 0.074) + (z_right * ((s_squared / len(x)) ** 0.5))
+
+    # Возвращаем кортеж левой и правой границ доверительного интервала
+    return lower_bound, upper_bound
