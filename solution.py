10bb2a61397bec2ffdd8d12fa31c7478297fbc6c
Velsee <113857212+Velsee@users.noreply.github.com>
2023-04-07 23:37:46 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,9 +7,7 @@ from scipy.stats import norm
 chat_id = 1253313260 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    b_mean = 4*x.mean() - 0.074
-    z = norm.ppf(1-p/2)
-    sd = x.std(ddof=1)
-    b_left = b_mean - z * sd / np.sqrt(len(x)) 
-    b_right = b_mean + z * sd / np.sqrt(len(x))
-    return (b_left, b_right)
+    b = 0.074 + (1 - 0.074) * p
+    b_min = 0.074 + (1 - 0.074) * np.min(x)
+    b_max = 0.074 + (1 - 0.074) * np.max(x)
+    return (b_min, b_max)
